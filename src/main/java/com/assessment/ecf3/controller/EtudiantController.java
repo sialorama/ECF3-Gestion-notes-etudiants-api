b707f6a8fd57f2c3e7691d5213f2a5619dbc4fc5
sialorama <sialorama@gmail.com>
2024-11-12 18:23:31 +0100

    update: README.md

--- src/main/java/com/assessment/ecf3/controller/EtudiantController.java
+++ src/main/java/com/assessment/ecf3/controller/EtudiantController.java
@@ -1,11 +1,11 @@
 package com.assessment.ecf3.controller;
 
 import com.assessment.ecf3.model.Etudiant;
-import com.assessment.ecf3.model.Note;
 import com.assessment.ecf3.service.EtudiantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/etudiants")
@@ -14,18 +14,15 @@ public class EtudiantController {
     @Autowired
     private EtudiantService etudiantService;
 
+    // Endpoint pour ajouter un étudiant (méthode POST)
     @PostMapping
-    public Etudiant ajouterEtudiant(@RequestBody Etudiant etudiant) {
-        return etudiantService.ajouterEtudiant(etudiant);
+    public ResponseEntity<Etudiant> ajouterEtudiant(@RequestBody Etudiant etudiant) {
+        Etudiant createdEtudiant = etudiantService.ajouterEtudiant(etudiant);
+        return new ResponseEntity<>(createdEtudiant, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public Etudiant getEtudiant(@PathVariable int id) {
         return etudiantService.getEtudiant(id);
-    }
-
-    @GetMapping("/{id}/notes")
-    public List<Note> consulterNotes(@PathVariable int id) {
-        return etudiantService.consulterNotes(id);
     }
 }
